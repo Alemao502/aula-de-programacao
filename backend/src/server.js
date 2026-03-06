5c916df6092f6b06769506e8ba9e1b382c0d01f5
TEU NOME AQUIPaulo <TEU EMAIL AQUIpauloroberto5002@gmail.com>
2026-03-06 01:05:03 -0300

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
   credentials: true
 }));
 
